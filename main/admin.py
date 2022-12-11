a76b6cec8016ab13be116d306c520c975f17e6f9
SamAdebisi <samadebisi12@gmail.com>
2022-12-11 19:26:19 +0100

    Product Catalog added

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
+from .models import Product, ProductImage, ProductTag
 
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'in_stock', 'price')
+    list_filter = ('active', 'in_stock', 'date_updated')
+    list_editable = ('in_stock',)
+    search_fields = ('name',)
+    prepopulated_fields = {"slug": ("name",)}
+
+
+class ProductTagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    list_filter = ('active',)
+    search_fields = ('name',)
+    prepopulated_fields = {"slug": ("name",)}
+    autocomplete_fields = ('products',)
+
+
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ('thumbnail_tag', 'product_name')
+    readonly_fields = ('thumbnail',)
+    search_fields = ('product_name',)
+
+    def thumbnail_tag(self, obj):
+        if obj.thumbnail:
+            return format_html(
+                '<img src="%s"/>' % obj.thumbnail.url
+            )
+        return "_"
+
+    thumbnail_tag.short_description = "Thumbnail"
+
+    def product_name(self, obj):
+        return obj.product.name
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductImage, ProductImageAdmin)
+admin.site.register(ProductTag, ProductTagAdmin)
--- main/models.py
+++ main/models.py
@@ -3,6 +3,11 @@ from django.db import models
 # Create your models here.
 
 
+class ActiveManager(models.Manager):
+    def active(self):
+        return self.filter(active=True)
+
+
 class Product(models.Model):
     name = models.CharField(max_length=32)
     description = models.TextField(blank=True)
@@ -11,3 +16,24 @@ class Product(models.Model):
     active = models.BooleanField(default=True)
     in_stock = models.BooleanField(default=True)
     date_updated = models.DateTimeField(auto_now_add=True)
+    objects = ActiveManager()
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="product-images")
+    thumbnail = models.ImageField(upload_to="product-thumbnails", null=True)
+
+
+class ProductTag(models.Model):
+    products = models.ManyToManyField(Product, blank=True)
+    name = models.CharField(max_length=32)
+    slug = models.SlugField(max_length=48)
+    description = models.TextField(blank=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
